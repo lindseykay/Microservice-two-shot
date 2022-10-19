d9d33a21b499841d8515b313f317e5fc704775da
Lindsey <lindseykaycarlson@outlook.com>
2022-10-19 16:42:21 -0700

    GET & POST requests to api_list_shoes are functional

--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -6,3 +6,17 @@ import json
 from common.json import ModelEncoder
 from .models import Shoe, BinVO
 # Create your views here.
+
+class ShoeListEncoder(ModelEncoder):
+    model = Shoe
+    properties = ["manufacturer"]
+
+@require_http_methods(["GET", "POST"])
+#"GET" is for showing a list of all shoes, "POST" is for creating a new shoe
+def api_list_shoes(request):
+    if request.method == "GET":
+        shoes = Shoe.objects.all()
+        return JsonResponse(
+            {"shoes": shoes},
+            encoder=ShoeListEncoder,
+        )
