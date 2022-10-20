59c7727ae17b347d388e835be26c223d48a1e745
Lindsey <lindseykaycarlson@outlook.com>
2022-10-20 15:50:39 -0700

    create new shoe form works, except picture_url won't clear

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
 import ShoesList from './ShoesList';
-import Shoe from './ShoeDetail';
+import ShoeForm from './ShoeForm.js';
 
 function App(props) {
   if (props.shoes === undefined) {
@@ -18,8 +18,8 @@ function App(props) {
           <Route path="/" element={<MainPage />} />
           <Route path="shoes">
             <Route path="" element={<ShoesList shoes={props.shoes} />} />
+            <Route path="new" element={<ShoeForm />} />
           </Route>
-          <Route path=":shoeId" element={<Shoe shoes={props.shoes} />} />
         </Routes>
       </div>
     </BrowserRouter>
