7760d96cb90f1ab5d2bf285c600299a45abe35ff
Lindsey <lindseykaycarlson@outlook.com>
2022-10-19 20:20:22 -0700

    shoes microservice successfully polling Bin data from wardrobe api

--- shoes/api/shoes_project/settings.py
+++ shoes/api/shoes_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "django_crontab",
     "shoes_rest.apps.ShoesApiConfig",
     "corsheaders",
     'django.contrib.admin',
@@ -132,3 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRONJOBS = [
+    ("* * * * *", "shoes_rest.get_bins"),
+]
